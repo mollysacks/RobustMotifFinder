1667c38024f0f1bb5004d87a36c1eff069cd111f
Alex Jambor <ajambor@ucsd.edu>
2023-03-01 12:04:38 -0800

    report runtime of arbitrary fxns

---
+++ TimingDecoratorPattern.py
@@ -0,0 +1,20 @@
+import time
+
+def ReportRuntime(f):
+    def g(*args):
+        t0 = time.time()
+        output = f(*args)
+        t1 = time.time()
+        if output:
+            return *output, t1-t0
+        else:
+            return t1-t0
+    return g
+
+@ReportRuntime
+def Test(a, b, c):
+    print(f"inputs are {a} {b} {c}")
+    return a, b, c
+
+
+Test(1, 2, 3)
